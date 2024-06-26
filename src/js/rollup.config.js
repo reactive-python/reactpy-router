f77f052dda00fa81b6288fcc584cc992a7945709
Archmonger <16909269+Archmonger@users.noreply.github.com>
2024-06-26 02:03:22 -0700

    change js output directory

--- src/js/rollup.config.js
+++ src/js/rollup.config.js
@@ -3,23 +3,23 @@ import commonjs from "rollup-plugin-commonjs";
 import replace from "rollup-plugin-replace";
 
 export default {
-  input: "src/index.js",
-  output: {
-    file: "../reactpy_router/bundle.js",
-    format: "esm",
-  },
-  plugins: [
-    resolve(),
-    commonjs(),
-    replace({
-      "process.env.NODE_ENV": JSON.stringify("production"),
-    }),
-  ],
-  onwarn: function (warning) {
-    if (warning.code === "THIS_IS_UNDEFINED") {
-      // skip warning where `this` is undefined at the top level of a module
-      return;
-    }
-    console.warn(warning.message);
-  },
+    input: "src/index.js",
+    output: {
+        file: "../reactpy_router/static/bundle.js",
+        format: "esm",
+    },
+    plugins: [
+        resolve(),
+        commonjs(),
+        replace({
+            "process.env.NODE_ENV": JSON.stringify("production"),
+        }),
+    ],
+    onwarn: function (warning) {
+        if (warning.code === "THIS_IS_UNDEFINED") {
+            // skip warning where `this` is undefined at the top level of a module
+            return;
+        }
+        console.warn(warning.message);
+    },
 };
