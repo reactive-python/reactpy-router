88d1f46a89a0b2d95cb87a4c8d6e1afed0891d40
Archmonger <16909269+Archmonger@users.noreply.github.com>
2024-10-06 14:52:47 -0700

    use server side prevent default

--- src/reactpy_router/components.py
+++ src/reactpy_router/components.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from typing import Any
 from uuid import uuid4
 
-from reactpy import component, html
+from reactpy import component, event, html
 from reactpy.backend.types import Location
 from reactpy.core.types import VdomChild, VdomDict
 from reactpy.web.module import export, module_from_file
@@ -26,15 +26,16 @@ def link(*children: VdomChild, to: str, **attributes: Any) -> VdomDict:
     # properly sets the location. When a client-server communication layer is added to a \
     # future ReactPy release, this component will need to be rewritten to use that instead. \
     set_location = _use_route_state().set_location
-    class_uuid = f"link-{uuid4().hex}"
 
+    @event(prevent_default=True)
     def on_click(_event: dict[str, Any]) -> None:
         pathname, search = to.split("?", 1) if "?" in to else (to, "")
         if search:
             search = f"?{search}"
         set_location(Location(pathname, search))
 
-    class_name = class_uuid
+    uuid_string = f"link-{uuid4().hex}"
+    class_name = f"{uuid_string}"
     if "className" in attributes:
         class_name = " ".join([attributes.pop("className"), class_name])
     # TODO: This can be removed when ReactPy stops supporting underscores in attribute names
@@ -45,9 +46,9 @@ def link(*children: VdomChild, to: str, **attributes: Any) -> VdomDict:
         **attributes,
         "href": to,
         "onClick": on_click,
-        "className": class_uuid,
+        "className": class_name,
     }
-    return html._(html.a(attrs, *children), html.script(link_js_content.replace("UUID", class_uuid)))
+    return html._(html.a(attrs, *children), html.script(link_js_content.replace("UUID", uuid_string)))
 
 
 def route(path: str, element: Any | None, *routes: Route) -> Route:
--- src/reactpy_router/static/link.js
+++ src/reactpy_router/static/link.js
@@ -1,7 +1,6 @@
 document.querySelector(".UUID").addEventListener(
   "click",
   (event) => {
-    event.preventDefault();
     let to = event.target.getAttribute("href");
     window.history.pushState({}, to, new URL(to, window.location));
   },
