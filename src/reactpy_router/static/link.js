f57b63be76817d295041b01f679979bb5bbdb910
Archmonger <16909269+Archmonger@users.noreply.github.com>
2024-10-06 14:29:53 -0700

    Fix browser history

--- src/reactpy_router/static/link.js
+++ src/reactpy_router/static/link.js
@@ -1,5 +1,9 @@
-document.querySelector(".UUID").addEventListener("click", (event) => {
-  event.preventDefault();
-  let to = event.target.getAttribute("href");
-  window.history.pushState({}, to, new URL(to, window.location));
-});
+document.querySelector(".UUID").addEventListener(
+  "click",
+  (event) => {
+    event.preventDefault();
+    let to = event.target.getAttribute("href");
+    window.history.pushState({}, to, new URL(to, window.location));
+  },
+  { once: true },
+);
