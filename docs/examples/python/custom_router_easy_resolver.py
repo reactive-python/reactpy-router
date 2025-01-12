e1f6f4765c7c90c5ea7d1f401f569a59bb0f052b
Archmonger <16909269+Archmonger@users.noreply.github.com>
2025-01-12 02:57:40 -0800

    Fix lint

--- docs/examples/python/custom_router_easy_resolver.py
+++ docs/examples/python/custom_router_easy_resolver.py
@@ -1,13 +1,15 @@
+from typing import ClassVar
+
 from reactpy_router.resolvers import ConversionInfo, ReactPyResolver
 
 
 # Create a custom resolver that uses the following pattern: "{name:type}"
 class CustomResolver(ReactPyResolver):
     # Match parameters that use the "<name:type>" format
-    param_pattern = r"<(?P<name>\w+)(?P<type>:\w+)?>"
+    param_pattern: str = r"<(?P<name>\w+)(?P<type>:\w+)?>"
 
     # Enable matching for the following types: int, str, any
-    converters = {
+    converters: ClassVar[dict[str, ConversionInfo]] = {
         "int": ConversionInfo(regex=r"\d+", func=int),
         "str": ConversionInfo(regex=r"[^/]+", func=str),
         "any": ConversionInfo(regex=r".*", func=str),
